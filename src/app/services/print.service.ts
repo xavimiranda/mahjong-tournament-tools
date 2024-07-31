5cca3a309a7c8167e2fd9405656093bd94a27dd6
xavimiranda <xavimiranda@gmail.com>
2024-07-31 22:51:57 +0100

    Add printService

---
+++ src/app/services/print.service.ts
@@ -0,0 +1,15 @@
+import { ElementRef, Injectable, viewChild } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrintService {
+
+  printElementRef(elem: ElementRef<HTMLElement> | undefined) {
+    if(!elem || !elem?.nativeElement) return
+    const originalContents = document.body.innerHTML
+    document.body.innerHTML = elem.nativeElement.innerHTML
+    window.print()
+    document.body.innerHTML = originalContents 
+  }
+}
