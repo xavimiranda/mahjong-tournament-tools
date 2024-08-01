b0a40e2f3edc08bc3500d176a12a7079bd64b851
xavimiranda <xavimiranda@gmail.com>
2024-08-01 17:12:37 +0100

    Restyle rounds component

--- src/app/seating/rounds/rounds.component.ts
+++ src/app/seating/rounds/rounds.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SeatingService } from '../seating.service';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @Component({
   selector: 'app-rounds',
   standalone: true,
-  imports: [NgxSpinnerModule],
+  imports: [NgxSpinnerModule, TooltipModule],
   templateUrl: './rounds.component.html',
   styleUrl: './rounds.component.scss',
 })
